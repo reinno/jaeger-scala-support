3ca1404da12edabb1321d0258ca5b53e6fb9748b
Wu, Lynn (Agoda) <lynn.wu@agoda.com>
2018-05-25 17:10:21 +0700

    init commit

---
+++ src/main/scala/io/reinno/QuickstartServer.scala
@@ -0,0 +1,26 @@
+package io.reinno
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+import akka.actor.{ ActorRef, ActorSystem }
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
+
+object QuickstartServer extends App with UserRoutes {
+
+  implicit val system: ActorSystem = ActorSystem("helloAkkaHttpServer")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  val userRegistryActor: ActorRef = system.actorOf(UserRegistryActor.props, "userRegistryActor")
+
+  lazy val routes: Route = userRoutes
+
+  Http().bindAndHandle(routes, "localhost", 8080)
+
+  println(s"Server online at http://localhost:8080/")
+
+  Await.result(system.whenTerminated, Duration.Inf)
+}
+
